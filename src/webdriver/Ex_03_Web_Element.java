592a52a2261e801f7358a033970faf6e14b3b7b3
TuAnh <letuanh2403@gmail.com>
2023-03-27 10:44:56 +0700

    update ex web element

--- src/webdriver/Ex_03_Web_Element.java
+++ src/webdriver/Ex_03_Web_Element.java
@@ -15,6 +15,14 @@ public class Ex_03_Web_Element {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
+	
+	By emailTextbox = By.id("mail");
+	By ageUnder18Radio = By.cssSelector("#under_18");
+	By educationTextAre = By.cssSelector("#edu");
+	By nameUser5Text = By.xpath("//h5[text()='Name: User5']");
+	By passwordTextbox = By.cssSelector("#disable_password");
+	By biographyTextArea = By.cssSelector("#bio");
+	By javaCheckbox = By.cssSelector("#java");
 
 	@BeforeClass
 	public void beforeClass() {
@@ -30,19 +38,93 @@ public class Ex_03_Web_Element {
 	}
 
 	@Test
-	public void TC_01_() {
+	public void TC_01_Displayed() {
+		driver.get("https://automationfc.github.io/basic-form/");
 		
+		//Textbox/ TextArea nếu có hiển thị nhập text vào và in ra console
+		if(driver.findElement(emailTextbox).isDisplayed()) {
+			driver.findElement(emailTextbox).sendKeys("selenium wedriver");
+			System.out.println("Email textbox displayed");
+		}else {
+			System.out.println("Email textbox is not displayed");
+		}
 		
+		//textarea
+		if(driver.findElement(educationTextAre).isDisplayed()) {
+			driver.findElement(educationTextAre).sendKeys("selenium GRID");
+			System.out.println("Education textarea displayed");
+		}else {
+			System.out.println("Education textarea is not displayed");
+		}
+		
+		//Radio
+		if(driver.findElement(ageUnder18Radio).isDisplayed()) {
+			driver.findElement(ageUnder18Radio).click();
+			System.out.println("Age under 18 is displayed");
+		}else {
+			System.out.println("Age under 18 is not displayed");
+		}
+		
+		if(driver.findElement(nameUser5Text).isDisplayed()) {
+			System.out.println("Name user 5 is displayed");
+		}else {
+			System.out.println("Name user 5 is not displayed");
+		}
 	}
 
 	@Test
-	public void TC_02_() {
+	public void TC_02_Enabale() {
+		driver.get("https://automationfc.github.io/basic-form/");
+		
+		if(driver.findElement(passwordTextbox).isEnabled()) {
+			System.out.println("Password textbox is enabale");
+		} else {
+			System.out.println("Password textbox is displayed");
+		}
+		
+		if(driver.findElement(biographyTextArea).isEnabled()) {
+			System.out.println("Biography textarea is enabale");
+		} else {
+			System.out.println("Biography textarea is displayed");
+		}
+		
+		if(driver.findElement(emailTextbox).isEnabled()) {
+			System.out.println("Email textbox is enabale");
+		} else {
+			System.out.println("Email textbox is displayed");
+		}
 	
 	}
 
 	@Test
-	public void TC_03_() {
+	public void TC_03_selected() {
+		driver.get("https://automationfc.github.io/basic-form/");
 		
+		//verify checkbox/ radio button are deselected
+		Assert.assertFalse(driver.findElement(ageUnder18Radio).isSelected());
+		Assert.assertFalse(driver.findElement(javaCheckbox).isSelected());
+		
+		//click to checkbox/ radio
+		driver.findElement(ageUnder18Radio).click();
+		driver.findElement(javaCheckbox).click();
+		sleepInSecond(2);
+		
+		//verify 
+		Assert.assertTrue(driver.findElement(ageUnder18Radio).isSelected());
+		Assert.assertTrue(driver.findElement(javaCheckbox).isSelected());
+		
+	}
+	@Test
+	public void TC_04_MailChimp() {
+		 
+	}
+	
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@AfterClass
