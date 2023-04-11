b928dfe4b4a6d30ec3b4c2ee9210195b3bb38d36
TuAnh <letuanh2403@gmail.com>
2023-04-11 10:28:33 +0700

    update topic7 dropdown

--- src/webdriver/Topic_07_Dropdown.java
+++ src/webdriver/Topic_07_Dropdown.java
@@ -39,6 +39,13 @@ public class Topic_07_Dropdown {
 	public void TC_01_() {
 		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
 		
+		selectItemInDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Slower");
+		
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("span#speed-button>span.ui-selectmenu-text")).getText(), "Slower");
+		
+		selectItemInDropdown("span#speed-button", "ul#speed-menu div[role='option']", "Faster");
+		
 		//1 - click vào 1 thẻ bất kì để xổ ra các item
 		driver.findElement(By.cssSelector("span#speed-button")).click();
 		
@@ -54,13 +61,10 @@ public class Topic_07_Dropdown {
 			
 			//ktra text của item đúng vs cái mình mong muốn
 			if(itemText.equals("Medium")) {
-				System.out.println("click vào item");
 				tempItem.click();
 				
 				//thoát ra khỏi vòng lặp ko xét các case còn lại nữa
 				break;
-			}else {
-				System.out.println("ko click vào item");
 			}
 		}
 		
@@ -77,6 +81,32 @@ public class Topic_07_Dropdown {
 		
 	}
 	
+	//Tránh lặp lại code nhiều lần chỉ cần gọi hàm ra để dùng
+	public void selectItemInDropdown(String parentCss, String allItemCss, String expectedTextItem) {
+		//1 - click vào 1 thẻ bất kì để xổ ra các item
+		driver.findElement(By.cssSelector(parentCss)).click();
+		
+		//2 - chờ tất cả các item đc load thành công
+		//lấy thẻ chứa text
+		explicitwait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(allItemCss)));
+		
+		List<WebElement> speedDropdownItems =  driver.findElements(By.cssSelector(allItemCss));
+		
+		for (WebElement tempItem : speedDropdownItems) {
+			String itemText = tempItem.getText();
+			System.out.println(itemText);
+			
+			//ktra text của item đúng vs cái mình mong muốn
+			if(itemText.equals(expectedTextItem)) {
+				tempItem.click();
+				
+				//thoát ra khỏi vòng lặp ko xét các case còn lại nữa
+				break;
+			}
+		}
+	}
+	
+
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000);
@@ -84,7 +114,6 @@ public class Topic_07_Dropdown {
 			e.printStackTrace();
 		}
 	}
-
 	@AfterClass
 	public void afterClass() {		
 		driver.quit();
