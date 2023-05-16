309bf852265348ac858b672a71ae18216b02b271
TuAnh <letuanh2403@gmail.com>
2023-05-16 16:03:13 +0700

    javascript executor

---
+++ src/webdriver/Topic_16_javascriptExecuter.java
@@ -0,0 +1,169 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_16_javascriptExecuter {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	JavascriptExecutor jsExecutor;
+
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+	}
+
+	//@Test
+	public void TC_01_tech_panda() {
+		driver.get("http://live.techpanda.org/");
+		//navigateToUrlByJS("http://live.techpanda.org/");
+		//sleepInSecond(5);
+		
+		Assert.assertEquals(getDomainName(), "live.techpanda.org");
+		Assert.assertEquals(executeForBrowser("return document.URL;"), "http://live.techpanda.org/");
+		
+		hightlightElement("//a[text()='Mobile']");
+		clickToElementByJS("//a[text()='Mobile']");
+		sleepInSecond(3);
+		
+		
+	}
+
+	@Test
+	public void TC_02_() {
+		driver.get("https://warranty.rode.com/login");
+		
+		String emailTextbox = "//input[@id='email']";
+		String passwordTextbox = "//input[@id='password']";
+		String loginButton = "//button[@type='submit']";
+		
+	
+		clickToElementByJS(loginButton);
+		sleepInSecond(2);
+		
+		Assert.assertEquals(getElementValidationMessage(emailTextbox), "Please fill out this field.");
+		
+		sendkeyToElementByJS(emailTextbox, "demo@123.com");
+		clickToElementByJS(loginButton);
+		sleepInSecond(3);
+		
+		Assert.assertEquals(getElementValidationMessage(passwordTextbox), "Please fill out this field.");
+		
+		
+	
+	}
+
+	@Test
+	public void TC_03_() {
+		
+	}
+	
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getDomainName() {
+		return (String) jsExecutor.executeScript("return document.domain;");
+	}
+	public Object executeForBrowser(String javaScript) {
+		return jsExecutor.executeScript(javaScript);
+	}
+
+	public String getInnerText() {
+		return (String) jsExecutor.executeScript("return document.documentElement.innerText;");
+	}
+
+	public boolean areExpectedTextInInnerText(String textExpected) {
+		String textActual = (String) jsExecutor.executeScript("return document.documentElement.innerText.match('" + textExpected + "')[0];");
+		return textActual.equals(textExpected);
+	}
+
+	public void scrollToBottomPage() {
+		jsExecutor.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+	}
+
+	public void navigateToUrlByJS(String url) {
+		jsExecutor.executeScript("window.location = '" + url + "'");
+		sleepInSecond(3);
+	}
+
+	public void hightlightElement(String locator) {
+		WebElement element = getElement(locator);
+		String originalStyle = element.getAttribute("style");
+		jsExecutor.executeScript("arguments[0].setAttribute('style', arguments[1])", element, "border: 2px solid red; border-style: dashed;");
+		sleepInSecond(2);
+		jsExecutor.executeScript("arguments[0].setAttribute('style', arguments[1])", element, originalStyle);
+	}
+
+	public void clickToElementByJS(String locator) {
+		jsExecutor.executeScript("arguments[0].click();", getElement(locator));
+		sleepInSecond(3);
+	}
+
+	public void scrollToElementOnTop(String locator) {
+		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", getElement(locator));
+	}
+
+	public void scrollToElementOnDown(String locator) {
+		jsExecutor.executeScript("arguments[0].scrollIntoView(false);", getElement(locator));
+	}
+	
+	public void setAttributeInDOM(String locator, String attributeName, String attributeValue) {
+		jsExecutor.executeScript("arguments[0].setAttribute('" + attributeName + "', '" + attributeValue +"');", getElement(locator));
+	}
+
+	public void removeAttributeInDOM(String locator, String attributeRemove) {
+		jsExecutor.executeScript("arguments[0].removeAttribute('" + attributeRemove + "');", getElement(locator));
+	}
+	
+	public void sendkeyToElementByJS(String locator, String value) {
+		jsExecutor.executeScript("arguments[0].setAttribute('value', '" + value + "')", getElement(locator));
+	}
+	
+	public String getAttributeInDOM(String locator, String attributeName) {
+		return (String) jsExecutor.executeScript("return arguments[0].getAttribute('" + attributeName + "');", getElement(locator));
+	}
+
+	public String getElementValidationMessage(String locator) {
+		return (String) jsExecutor.executeScript("return arguments[0].validationMessage;", getElement(locator));
+	}
+
+	public boolean isImageLoaded(String locator) {
+		boolean status = (boolean) jsExecutor.executeScript(
+				"return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0", getElement(locator));
+		return status;
+	}
+
+	public WebElement getElement(String locator) {
+		return driver.findElement(By.xpath(locator));
+	}
+
+	@AfterClass
+	public void afterClass() {		
+		driver.quit();
+		
+	}
+}
